aa32b95d93422e06876d3d99c487911ff097dcb6
leads <leads@DevBox>
2022-10-26 14:32:56 -0400

    Create my own hash map function and map values to my hash map.

---
+++ project_java/src/project_java/myHashSet.java
@@ -0,0 +1,48 @@
+package project_java;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class myHashSet {
+	//Set the percent to divide by
+	private int numBuckets = 15000;
+	//Create the List
+    List<Integer>[] buckets;
+    
+    //Hash the key using the numbuckets
+    private int hash_function(int key){
+        return key % numBuckets;
+    }
+    
+    //set buckets to like list with index of numbuckets
+    public void MyHashSet() {
+       buckets = new LinkedList[numBuckets];
+    }
+    
+    public void add(int key) {
+      //set i to the hash function
+      int i  = hash_function(key);
+      //check if bucket is empty and create it
+      if(buckets[i] == null) buckets[i] = new LinkedList<>();
+      //else add key
+      if(buckets[i].indexOf(key) == -1)
+          buckets[i].add(key);
+    }
+    
+    public void remove(int key) {
+      //set i to the hash function
+      int i  = hash_function(key);
+      //if bucket is empty return
+      if(buckets[i] == null) return;
+      //if bucket is not empty and remove bucket using key
+      if(buckets[i].indexOf(key) != -1) buckets[i].remove(buckets[i].indexOf(key));
+    }
+    
+    public boolean contains(int key) {
+    	 //set i to the hash function
+        int i  = hash_function(key);
+        //check if null or value does not exist
+        if(buckets[i] == null || buckets[i].indexOf(key) == -1) return false;
+        return true;
+    }
+}
